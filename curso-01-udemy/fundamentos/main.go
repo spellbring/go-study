6639d9fe65c8878a5640aa7f276383da90be77b6
Jaime Reyes <jreyesr@falabella.cl>
2021-11-18 09:03:38 -0300

    adding readme.md with basic concepts

--- curso-01-udemy/fundamentos/main.go
+++ curso-01-udemy/fundamentos/main.go
@@ -14,11 +14,15 @@ func main() {
 
 	fmt.Printf("Valores booleanos\n")
 	varBooleans()
-	fmt.Printf("Fin Valores booleanos")
+	fmt.Printf("Fin Valores booleanos\n")
 
 	fmt.Printf("Valores enteros\n")
 	varIntegers()
-	fmt.Printf("Fin Valores enteros")
+	fmt.Printf("Fin Valores enteros\n")
+
+	fmt.Printf("cadenas de bytes\n")
+	varStrings()
+	fmt.Printf("Fin cadena de bytes\n")
 
 }
 
@@ -40,5 +44,33 @@ func varIntegers() {
 	y := 42.2
 	fmt.Printf("El tipo de este valor es: %T y el valor es: %v\n", x, x)
 	fmt.Printf("El tipo de este valor es: %T\n", y)
+
+}
+
+func varStrings() {
+
+	s1 := "Hola mundo"
+	s2 := `Esta es una linea
+		partida`
+
+	fmt.Printf("El tipo de s1 es: %T y su valor es %v\n", s1, s1)
+	fmt.Printf("El tipo de s2 es: %T y su valor es %v\n", s2, s2)
+
+	bs := []byte(s1)
+	fmt.Println(bs)
+	fmt.Printf("%T\n", bs)
+
+	fmt.Println("")
+
+	//Valores en Hexa
+	for i := 0; i < len(s1); i++ {
+		fmt.Printf("%#U ", s1[i])
+	}
+
+	fmt.Println("")
+
+	for i, v := range s1 {
+		fmt.Printf("El indice %d el valor es %v\n", i, v)
+	}
 
 }
